8e51c55c3b469ac0c691d2a852e176370047839f
Rodion Myastkowskuy <rodion.myastkowskuy@gmail.com>
2023-01-11 17:17:08 +0200

    adds 4 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import { Profile } from 'components/profile/Profile';
 import { StatisticTitle } from 'components/statistic/StatisticTitle';
 import { Statistics } from 'components/statistic/Statistics';
 import { FriendList } from 'components/friends/FriendList';
+import { TransactionHistory } from 'components/transaction/TransactionHistory';
 import user from './to/user.json';
 import data from './to/data.json';
 import friends from './to/friends.json';
+import transactions from './to/transactions.json';
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
         <Statistics stats={data} />
       </StatisticTitle>
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/transaction/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transaction_history}>
+      <thead className={css.thead}>
+        <tr>
+          <th className={css.title}>Type</th>
+          <th className={css.title}>Amount</th>
+          <th className={css.title}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.roll} key={id}>
+            <td className={css.cell}>{type}</td>
+            <td className={css.cell}>{amount}</td>
+            <td className={css.cell}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+    })
+  ),
+};
